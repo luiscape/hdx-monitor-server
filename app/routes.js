52228288f8fa3a8ce0554652232ab2893fe208db
Luis Capelo <luiscape@gmail.com>
2015-08-05 20:54:44 -0400

    adding a connection to the DataStore service

--- app/routes.js
+++ app/routes.js
@@ -1,28 +1,43 @@
-module.exports = function(app, passport) {
+module.exports = function (app, passport) {
+
+  var NProgress = require('nprogress')
+  var http = require('http')
+
 
   // show the home page (will also have our login links)
-  app.get('/', isLoggedOut, function(req, res) {
+  app.get('/', isLoggedOut, function (req, res) {
     res.render('index.ejs')
   })
 
+  app.post('/datastore', function (req, res) {
+    // NProgress.start()
+    var datastore = 'http://localhost:' + process.env.DATASTORE_PORT + '/show/' + req.body.resourceid
+    console.log(datastore)
+    http.get(datastore, function (response) {
+      response.on('data', function(data) {
+        res.send(JSON.parse(data))
+      })
+    })
+  })
+
   // PROFILE SECTION =========================
-  app.get('/profile', isLoggedIn, function(req, res) {
+  app.get('/profile', isLoggedIn, function (req, res) {
     res.render('profile.ejs', {
-      user : req.user
+      user: req.user
     })
   })
 
   // LOGOUT ==============================
-  app.get('/logout', isLoggedIn, function(req, res) {
+  app.get('/logout', isLoggedIn, function (req, res) {
     req.logout()
     res.redirect('/')
   })
 
-  app.get('/dashboard', isLoggedIn, function(req, res) {
+  app.get('/dashboard', isLoggedIn, function (req, res) {
     res.render('dashboard.ejs')
   })
 
-  app.get('/datastore', isLoggedIn, function(req, res) {
+  app.get('/datastore', isLoggedIn, function (req, res) {
     res.render('datastore.ejs')
   })
 
@@ -33,28 +48,28 @@ module.exports = function(app, passport) {
   // locally --------------------------------
   // LOGIN ===============================
   // show the login form
-  app.get('/login', isLoggedOut, function(req, res) {
+  app.get('/login', isLoggedOut, function (req, res) {
     res.render('login.ejs', { message: req.flash('loginMessage') })
   })
 
   // process the login form
   app.post('/login', isLoggedOut, passport.authenticate('local-login', {
-    successRedirect : '/dashboard', // redirect to the secure profile section
-    failureRedirect : '/login', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
+    successRedirect: '/dashboard', // redirect to the secure profile section
+    failureRedirect: '/login', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
   }))
 
   // SIGNUP =================================
   // show the signup form
-  app.get('/signup', isLoggedOut, function(req, res) {
+  app.get('/signup', isLoggedOut, function (req, res) {
     res.render('signup.ejs', { message: req.flash('signupMessage') })
   })
 
   // process the signup form
   app.post('/signup', isLoggedOut, passport.authenticate('local-signup', {
-    successRedirect : '/profile', // redirect to the secure profile section
-    failureRedirect : '/signup', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
+    successRedirect: '/profile', // redirect to the secure profile section
+    failureRedirect: '/signup', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
   }))
 
   // =============================================================================
@@ -62,16 +77,16 @@ module.exports = function(app, passport) {
   // =============================================================================
 
   // locally --------------------------------
-  app.get('/connect/local', isLoggedIn, function(req, res) {
+  app.get('/connect/local', isLoggedIn, function (req, res) {
     res.render('connect-local.ejs', { message: req.flash('loginMessage') })
   })
 
-  app.post('/connect/local', isLoggedIn, 
-   passport.authenticate('local-signup', {
-    successRedirect : '/profile', // redirect to the secure profile section
-    failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
-   })
+  app.post('/connect/local', isLoggedIn,
+    passport.authenticate('local-signup', {
+      successRedirect: '/profile', // redirect to the secure profile section
+      failureRedirect: '/connect/local', // redirect back to the signup page if there is an error
+      failureFlash: true // allow flash messages
+    })
   )
 
   //
