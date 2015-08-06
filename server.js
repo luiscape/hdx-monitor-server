37812be4392e729b317ba71046368ea1c5ee240c
Luis Capelo <luiscape@gmail.com>
2015-08-06 11:11:02 -0400

    reorganizing repo and adding angular

--- server.js
+++ server.js
@@ -3,43 +3,43 @@
 // set up ======================================================================
 // get all the tools we need
 
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 8080;
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash    = require('connect-flash');
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 8080
+var mongoose = require('mongoose')
+var passport = require('passport')
+var flash = require('connect-flash')
+var methodOverride = require('method-override')
 
-var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
+var morgan = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var session = require('express-session')
 
-var configDB = require('./config/database.js');
+var configDB = require('./config/database.js')
 
 // configuration ===============================================================
-console.log(configDB.url)
-mongoose.connect(configDB.url); // connect to our database
+mongoose.connect(configDB.url) // connect to our database
 
-require('./config/passport')(passport); // pass passport for configuration
+require('./config/passport')(passport) // pass passport for configuration
 
 // set up our express application
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
-app.use(express.static('static')); // serving static files.
+app.use(morgan('dev')) // log every request to the console
+app.use(cookieParser()) // read cookies (needed for auth)
+app.use(bodyParser()) // get information from html forms
+app.use(express.static('public')) // serving static files.
 
-app.set('view engine', 'ejs'); // set up ejs for templating
+app.set('view engine', 'ejs') // set up ejs for templating
 
 // required for passport
-app.use(session({ secret: process.env.SESSION_PASSWORD || 'devpassword' })); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
+app.use(session({ secret: process.env.SESSION_PASSWORD || 'devpassword' })) // session secret
+app.use(passport.initialize())
+app.use(passport.session()) // persistent login sessions
+app.use(flash()) // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./app/routes.js')(app, passport) // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
-app.listen(port);
-console.log('HDX Monitor Server running on port ' + port);
+app.listen(port)
+console.log('HDX Monitor Server running on port ' + port)
