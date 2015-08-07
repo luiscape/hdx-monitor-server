751a8a8f8bfbe4016c1ef29322525183fced35d9
Luis Capelo <luiscape@gmail.com>
2015-08-07 13:11:55 -0400

    adding service port for funnel

--- bin/run.sh
+++ bin/run.sh
@@ -8,6 +8,7 @@ export MONGODB_USER_PASSWORD=Dp1mhPZNj2
 export MONGODB_DATABASE=hdxmonitor
 export MONGODB_LOCATION_URL=172.17.0.5:27017  # relative
 export DATASTORE_PORT=5000  # default
+export FUNNEL_STATS_PORT=7000  #default
 
 #
 # Running application in development environment.
