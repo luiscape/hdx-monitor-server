2d6105d63fe934998fc90bec68536c3faa6324f6
Luis Capelo <luiscape@gmail.com>
2015-08-06 20:50:11 -0400

    adapting to JS standard

--- public/controllers/status_alert.js
+++ public/controllers/status_alert.js
@@ -1,25 +1,20 @@
-// script to manage the alert system
-// at the top of the page of the Ebola API
-
-// function for checking if string is null, empty, or undefined
-// from: http://stackoverflow.com/questions/154059/how-do-you-check-for-an-empty-string-in-javascript
-function isEmpty (str) {
-  return (!str || 0 === str.length)
-}
-
-// function to display system alerts
+//
+// Displays the status of a
+// service on the service's
+// respective page.
+//
 function displayAlert (alertType, alertMessage) {
-  // defining the two variables used
-  var alertMessage, alertType
-
-  // control for alert type to html
+  //
+  // Control for alert type to html
   // 0 = simple (green)
   // 1 = mild (yellow)
   // 2 = severe (red)
+  //
   if (alertType && alertMessage) {
-    colors = ['#cccccc', '#404040', '#404040']
-    alertColor = colors[alertType]
-    if (alertType == 0) {
+    var alertIcon = null
+    var colors = ['#cccccc', '#404040', '#404040']
+    var alertColor = colors[alertType]
+    if (alertType === 0) {
       alertIcon = '<span style="color:#cccccc;" class="fa fa-check"></span>'
     } else {
       alertIcon = '<span style="color:#cccccc;" class="fa fa-warning"></span>'
@@ -28,13 +23,13 @@ function displayAlert (alertType, alertMessage) {
       '<span href="#">' + alertMessage + '</span>' + '</span>'
 
     // pulse only for the mild and severe alerts
-    if (alertType == 1 | alertType == 2) {
-      pulse = '<div class="outer"></div>'
+    if (alertType === 1 | alertType === 2) {
+      var pulse = '<div class="outer"></div>'
       var pulseDoc = document.getElementById('alert-container')
       pulseDoc.innerHTML = pulse
     }
   } else {
-    alertContainer = '<span class="fa fa-warning"></span>' +
+    var alertContainer = '<span class="fa fa-warning"></span>' +
       '<span ' + 'style="color:' + 'grey' + '">' +
       'unavailable' + '</span>'
   }
