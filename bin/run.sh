1a1c3c0176f8b6d19804dcaffdd2f893425eccac
Luis Capelo <luiscape@gmail.com>
2015-08-14 09:04:31 -0400

    removing mongo call

--- bin/run.sh
+++ bin/run.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 #
-# Creates an user in the
-# MongoDB instance.
-#
-mongo $MONGODB_DATABASE --eval "db.createUser({user: '$MONGODB_USER_NAME', pwd: '$MONGODB_USER_PASSWORD', roles: [{ role: 'readWrite', db: '$MONGODB_DATABASE' },]})"
-
-
-#
 # Running application in development environment.
 #
 pm2 start server.js --no-daemon
