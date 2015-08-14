a94c5dd644305bbfa368a2183eba102c37e88117
Luis Capelo <luiscape@gmail.com>
2015-08-14 09:03:24 -0400

    linting

--- bin/configure_mongo.sh
+++ bin/configure_mongo.sh
@@ -4,4 +4,4 @@
 # Creates an user in the
 # MongoDB instance.
 #
-mongo $MONGODB_DATABASE --eval "db.createUser({user: '$MONGODB_USER_NAME', pwd: '$MONGODB_USER_PASSWORD', roles: [{ role: 'readWrite', db: '$MONGODB_DATABASE' },]})"
+mongo $MONGO_PORT_27017_TCP_ADDR:27017/$MONGODB_DATABASE --eval "db.createUser({user: '$MONGODB_USER_NAME', pwd: '$MONGODB_USER_PASSWORD', roles: [{ role: 'readWrite', db: '$MONGODB_DATABASE' },]})"
