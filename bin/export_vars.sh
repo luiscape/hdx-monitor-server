54e7a2539619259272b6be041c8998ae2299ab5d
Luis Capelo <luiscape@gmail.com>
2015-08-24 17:58:34 -0400

    exporting variables for local work

---
+++ bin/export_vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#
+# Export variables useful
+# for development.
+#
+export MONGODB_DATABASE=test
+export MONGODB_USER_NAME=foo
+export MONGODB_USER_PASSWORD=bar
+export MONGO_PORT_27017_TCP_ADDR=127.0.0.1
