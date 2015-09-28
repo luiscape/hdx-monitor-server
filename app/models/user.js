312092005560883eac4bdf5ea7567072f868f6e0
Luis Capelo <luiscape@gmail.com>
2015-09-28 14:56:11 -0400

    improving comments and linting

--- app/models/user.js
+++ app/models/user.js
@@ -1,30 +1,33 @@
-// app/models/user.js
-// load the things we need
-var mongoose = require('mongoose');
-var bcrypt   = require('bcrypt-nodejs');
+var mongoose = require('mongoose')
+var bcrypt = require('bcrypt-nodejs')
 
-// define the schema for our user model
+//
+// Define schema of user.
+//
 var userSchema = mongoose.Schema({
+  local: {
+    name: String,
+    email: String,
+    password: String,
+    apikey: String
+  }
 
-    local : {
-        name : String,
-        email : String,
-        password : String,
-        apikey : String
-    }
+})
 
-});
+//
+// Generate hash of password.
+//
+userSchema.methods.generateHash = function (password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
+}
 
-// methods ======================
-// generating a hash
-userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
+//
+// Check if the password is valid.
+// In this case, we are not performing
+// any proper checks.
+//
+userSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.local.password)
+}
 
-// checking if password is valid
-userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
-};
-
-// create the model for users and expose it to our app
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema)
