9a7abb4ce60b15259c788f0923efd3a544aba459
Luis Capelo <luiscape@gmail.com>
2015-09-28 16:00:13 -0400

    linting

--- server.js
+++ server.js
@@ -1,46 +1,51 @@
+var morgan = require('morgan')
 var express = require('express')
 var mongoose = require('mongoose')
 var passport = require('passport')
 var flash = require('connect-flash')
-var methodOverride = require('method-override')
-
-var morgan = require('morgan')
 var bodyParser = require('body-parser')
 var session = require('express-session')
 var cookieParser = require('cookie-parser')
+var methodOverride = require('method-override')
 
+//
+// Change configuration files here.
+//
+var config = require('./config/dev.js')
 var configDB = require('./config/database.js')
 
 //
-// Change configuration file here.
-//
-var config = require('./config/dev.js')
-
-//
-// App variables.
+// Create Express application.
 //
 var app = express()
 
-mongoose.connect(configDB.url)
-
-require('./config/passport')(passport)  // Pass passport for configuration.
+//
+// Application configuration.
+//
 app.use(morgan('dev'))
 app.use(cookieParser())
 app.use(bodyParser())
 app.use(express.static('public'))
-
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 
 //
-// Required for passport.
+// Load passport and routes.
+//
+require('./config/passport')(passport)
+require('./server/routes.js')(app, passport)
+
+//
+// Configure passport.
 //
 app.use(session({ secret: process.env.SESSION_PASSWORD || 'devpassword' }))
 app.use(passport.initialize())
-app.use(passport.session())  // Persistent login sessions.
+app.use(passport.session())
 app.use(flash())
 
-require('./server/routes.js')(app, passport)
-
+//
+// Connect to database and start application.
+//
+mongoose.connect(configDB.url)
 app.listen(config.port)
 console.log('HDX Monitor Server (' + config.version + ') running on port ' + config.port)
