c46dcc9d0edff69f89f159131d69d8c67b9d2a64
Luis Capelo <luiscape@gmail.com>
2015-09-28 15:20:11 -0400

    adding port

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,6 @@ module.exports = {
   'CkanInstance': 'https://data.hdx.rwlabs.org/',
   'DefaultApiKey': process.env.DEFAULT_API_KEY,
   'version': 'v.0.1.6',
-  'repository': 'https://github.com/luiscape/hdx-monitor-server'
+  'repository': 'https://github.com/luiscape/hdx-monitor-server',
+  'port': process.env.PORT || 8080
 }
