df567579215c47f01e4c98a4b007e4ef189180ac
Luis Capelo <luiscape@gmail.com>
2015-09-30 17:15:50 -0400

    bumping to 0.1.7

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,17 @@
 module.exports = {
   'CkanInstance': 'https://data.hdx.rwlabs.org/',
-  'DefaultApiKey': process.env.DEFAULT_API_KEY,
-  'version': 'v.0.1.6',
+  'DefaultApiKey': process.env.DEFAULT_API_KEY || null,
+  'version': 'v.0.1.7',
   'repository': 'https://github.com/luiscape/hdx-monitor-server',
-  'port': process.env.PORT || 8080
+  'port': process.env.PORT || 8080,
+  'services': {
+    'datastore': {
+      'host': '',
+      'port': ''
+    },
+    'orgstats': {
+      'host': 'localhost',
+      'port': '2000'
+    }
+  }
 }
