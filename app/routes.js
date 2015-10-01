6597cffb6a8308cf2ca703d2b257ff4803350829
Luis Capelo <luiscape@gmail.com>
2015-10-01 16:05:07 -0400

    adding 404 routes

--- app/routes.js
+++ app/routes.js
@@ -238,7 +238,8 @@ module.exports = function (app, passport, config) {
   // TODO: Create 404 page.
   //
   app.use(function (req, res, next) {
-    res.status(404).redirect('/')
+    res.status(404)
+    res.render('404.ejs')
   })
 
 }
